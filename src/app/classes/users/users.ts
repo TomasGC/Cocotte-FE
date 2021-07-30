cf94b76b0c262747e50e1f294fe92cc87047963b
TomasGC <tom.gcardoso@ecomail.pro>
2021-07-30 15:36:31 +0200

    Removed useless translation in users.ts.

--- src/app/classes/users/users.ts
+++ src/app/classes/users/users.ts
@@ -28,17 +28,4 @@ export class Users {
     public dailyMeals: Array<DailyMeals>;
 
     constructor() {}
-
-    static GetDay(day){
-      switch (Number.parseInt(DayOfWeek[day])) {
-        case DayOfWeek.Sunday: return "Dimanche";
-        case DayOfWeek.Monday: return "Lundi";
-        case DayOfWeek.Tuesday: return "Mardi";
-        case DayOfWeek.Wednesday: return "Mercredi";
-        case DayOfWeek.Thursday: return "Jeudi";
-        case DayOfWeek.Friday: return "Vendredi";
-        case DayOfWeek.Saturday: return "Samedi";
-        default: return "";
-      }
-    }
   }
